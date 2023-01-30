bdd76b06bc4db1c0f705677e38253ed42ebb0420
Ketan <ketan992132@gmail.com>
2023-01-30 13:38:27 +0530

    Commit message

--- com/methods/Main.java
+++ com/methods/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args) {
         sum();
     }
+    /*
+            syntax of methods:-
+         --------------------------
+         return_type name(){
+                // body
+                return statement;
+         }
+     */
     static void sum(){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter num1:- ");
