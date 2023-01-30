d4e00bf26bda185939943885536f299653f41d2d
Ketan <ketan992132@gmail.com>
2023-01-30 13:42:02 +0530

    Commit message

--- com/methods/Greeting.java
+++ com/methods/Greeting.java
@@ -1,7 +1,17 @@
 package com.methods;
 
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Enter your name= ");
+        String name = in.next();
+
+        System.out.println("My name is "+name);
     }
 }
