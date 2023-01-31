5a54813cac66669550e842eca88c3656b9e3a822
Ketan <ketan992132@gmail.com>
2023-01-31 07:45:04 +0530

    Commit message

--- com/methods/Main.java
+++ com/methods/Main.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         sum();
+
         int ans = sum2();
         System.out.println("The sum of two number is= "+ans);
+
+        int ans1 = sum3(23,467);
+        System.out.println("The sum of two number is= "+ans1);
     }
     /*
             syntax of methods:-
@@ -26,6 +30,7 @@ public class Main {
         int ans = num1+num2;
         System.out.println("The sum of two number is= "+ans);
     }
+
     // return the value
     static int sum2(){
         Scanner in = new Scanner(System.in);
@@ -34,5 +39,10 @@ public class Main {
         System.out.print("Enter num2:- ");
         int num2 = in.nextInt();
         return num1+num2;
+    }
+
+    // pass the argument in the function
+    static int sum3(int a, int b){
+        return a+b;
     }
 }
