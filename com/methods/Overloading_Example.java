a4be40c7c4badcc165e4dcc1537fa7d6f2aac74a
Ketan <ketan992132@gmail.com>
2023-02-01 21:28:26 +0530

    Commit message

--- com/methods/Overloading_Example.java
+++ com/methods/Overloading_Example.java
@@ -1,5 +1,7 @@
 package com.methods;
 
+import java.util.Arrays;
+
 public class Overloading_Example {
     public static void main(String[] args) {
         int ans = sum(3,6);
@@ -7,6 +9,9 @@ public class Overloading_Example {
 
         int ans1 = sum(3,6,7);
         System.out.println("The sum of two number= "+ans1);
+
+        demo(3,4,5,6,7,44,46);
+        demo("Ketan","kelvin","dipesh","mother","father");
     }
     static int sum(int num1, int num2){
         return num1+num2;
@@ -15,4 +20,13 @@ public class Overloading_Example {
     static int sum(int num1, int num2, int num3){
         return num1+num2+num3;
     }
+
+    static void demo(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void demo(String ...v){
+        System.out.println(Arrays.toString(v));
+    }
+
 }
