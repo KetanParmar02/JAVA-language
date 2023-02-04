4a12d3466fa008f48c6f8b8383d9a77fd4d47f0f
Ketan <ketan992132@gmail.com>
2023-02-04 14:14:56 +0530

    Commit message

--- com/Array/Max_Value_In_1D_Array.java
+++ com/Array/Max_Value_In_1D_Array.java
@@ -18,10 +18,17 @@ public class Max_Value_In_1D_Array {
         System.out.println(Arrays.toString(arr));
 
         float ans = Max_Value(arr);
-        System.out.print("Max Value in 1D Array= "+ans);
+        System.out.println("Max Value in 1D Array= "+ans);
+
+        float ans1 = Max(arr,1,4);
+        System.out.print("Max Value in given Array range= "+ans1);
     }
     static float Max_Value(float[] nums){
         float max_value = Float.MIN_VALUE;
+
+        if (nums.length == 0){
+            return -1;
+        }
 //        for (int i = 0; i < nums.length; i++) {
 //            if (nums[i] > max_value){
 //                max_value = nums[i];
@@ -32,6 +39,22 @@ public class Max_Value_In_1D_Array {
         for (float ans : nums) {
             if (ans > max_value) {
                 max_value = ans;
+            }
+        }
+        return max_value;
+    }
+
+    // find max value in range
+    static float Max(float[] nums, int start, int end){
+        float max_value = Float.MIN_VALUE;
+
+        if (nums.length == 0){
+            return -1;
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (nums[i] > max_value){
+                max_value = nums[i];
             }
         }
         return max_value;
