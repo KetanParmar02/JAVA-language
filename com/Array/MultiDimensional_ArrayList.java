49984eb1cc4780b05a1a8403ca339e0b9cc3a699
Ketan <ketan992132@gmail.com>
2023-02-05 08:34:29 +0530

    Commit message

---
+++ com/Array/MultiDimensional_ArrayList.java
@@ -0,0 +1,28 @@
+package com.Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiDimensional_ArrayList {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter Arraylist Element= ");
+        ArrayList<ArrayList<Double>> list = new ArrayList<>();
+
+        // initialize the how many size of the ArrayList
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // add the Arraylist Element in the 2D arraylist
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add((Math.random()*100));
+            }
+        }
+
+        System.out.println(list);
+    }
+}
