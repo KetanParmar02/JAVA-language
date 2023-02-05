a6d78fc34f28828c90a5148407c73f32f4e17f94
Ketan <ketan992132@gmail.com>
2023-02-05 12:20:26 +0530

    Commit message

---
+++ com/strings_and_string_bulider/String_Builder.java
@@ -0,0 +1,15 @@
+package com.strings_and_string_bulider;
+
+public class String_Builder {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a'+ i);
+            builder.append(ch);
+        }
+
+        System.out.println(builder);
+        System.out.println(builder.reverse());
+    }
+}
