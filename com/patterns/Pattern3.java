add7dbb01ae47dd65a2a08faad3d860c97452003
Ketan <ketan992132@gmail.com>
2023-02-05 20:49:13 +0530

    Commit message

---
+++ com/patterns/Pattern3.java
@@ -0,0 +1,21 @@
+package com.patterns;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        pattern3(5);
+    }
+    // pattern
+    //    * * * * *
+    //    * * * *
+    //    * * *
+    //    * *
+    //    *
+    static void pattern3(int n){
+        for (int row = n; row >= 1; row--) {
+            for (int col = row; col >= 1; col--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
