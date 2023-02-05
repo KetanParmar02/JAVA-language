00c9654c1bcd0501822a8163c683285cc2a656c8
Ketan <ketan992132@gmail.com>
2023-02-05 20:42:54 +0530

    Commit message

---
+++ com/patterns/Pattern2.java
@@ -0,0 +1,15 @@
+package com.patterns;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        pattern2(10);
+    }
+    static void pattern2(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
