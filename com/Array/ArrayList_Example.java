ea0430db0e4eafb642a090a9f6d955752f5ac308
Ketan <ketan992132@gmail.com>
2023-02-05 08:16:46 +0530

    Commit message

--- com/Array/ArrayList_Example.java
+++ com/Array/ArrayList_Example.java
@@ -1,25 +1,38 @@
 package com.Array;
 
+import java.net.SecureCacheResponse;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayList_Example {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
         // syntax of an ArrayList
         ArrayList<Integer> list = new ArrayList<>(5);
 
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+//        list.add(6);
+//        list.add(7);
+//
+//        System.out.println(list);
+//
+//        list.set(5,76);
+//        System.out.println(list.hashCode());
+//        System.out.println(list.contains(2));
+//        System.out.println(list);
 
-        System.out.println(list);
+        // input
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
 
-        list.set(5,76);
-        System.out.println(list.hashCode());
-        System.out.println(list.contains(2));
-        System.out.println(list);
+        // output print
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
