3ced27a650e00e27e4c30fd284522ffece14daab
Ketan <ketan992132@gmail.com>
2023-02-05 08:08:21 +0530

    Commit message

---
+++ com/Array/ArrayList_Example.java
@@ -0,0 +1,20 @@
+package com.Array;
+
+import java.util.ArrayList;
+
+public class ArrayList_Example {
+    public static void main(String[] args) {
+
+        // syntax of an ArrayList
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        System.out.println(list);
+    }
+}
