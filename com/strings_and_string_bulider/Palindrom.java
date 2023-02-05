6f0a00162a5517aaf1144bd955b243c6394e9ef3
Ketan <ketan992132@gmail.com>
2023-02-05 12:34:37 +0530

    Commit message

---
+++ com/strings_and_string_bulider/Palindrom.java
@@ -0,0 +1,27 @@
+package com.strings_and_string_bulider;
+
+public class Palindrom {
+    public static void main(String[] args) {
+        String name = "abcdba";
+        boolean ans = isPalindrom(name);
+        System.out.println(ans);
+    }
+    static boolean isPalindrom(String str){
+
+        if (str == null || str.length() == 0){
+            return true;
+        }
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i <= str.length()/2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+
+            if (start != end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
