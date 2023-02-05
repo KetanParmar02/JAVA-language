ad5f958b5d9049cb4d4d6f0b61f7789bb985b336
Ketan <ketan992132@gmail.com>
2023-02-05 10:39:55 +0530

    Commit message

--- com/strings_and_string_bulider/Main.java
+++ com/strings_and_string_bulider/Main.java
@@ -3,5 +3,13 @@ package com.strings_and_string_bulider;
 public class Main {
     public static void main(String[] args) {
 
+        String name = "ketan";
+        String name1 = "ketan";
+
+        String name2 = new String("ketan");
+        String name3 = new String("ketan");
+
+        System.out.println(name2 == name3);
+        System.out.println(name.equals(name1));
     }
 }
