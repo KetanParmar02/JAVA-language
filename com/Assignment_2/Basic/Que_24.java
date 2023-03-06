8f5ac5474460583ec57c05bceb8c7c68447406af
Ketan <ketan992132@gmail.com>
2023-03-06 12:34:20 +0530

    Commit message

--- com/Assignment_2/Basic/Que_24.java
+++ com/Assignment_2/Basic/Que_24.java
@@ -5,28 +5,13 @@ import java.util.Scanner;
 public class Que_24 {
     public static void main(String[] args) {
 
-        //
-        Scanner sc = new Scanner(System.in);
-////        int num, largest_num=0;
-////        System.out.print("ENTER A NUMBER: ");
-////        num = sc.nextInt();
-//
-//        // Condition Starts
-//        while(num!=0){
-//            System.out.print("ENTER A NUMBER: ");
-//            num = sc.nextInt();
-//            System.out.println("ENTER 0 TO END LOOP");
-//            if(num>largest_num){
-//                largest_num = num; // The value of largest_num will change to the value of num
-//            }
-//        }// Condition Ends
-//        System.out.print("The largest number entered by the user is: "+largest_num);
-
+        // Take integer inputs till the user enters 0 and print the largest number from all.
+        Scanner in = new Scanner(System.in);
         int num, largest_num=0;
 
         do {
             System.out.print("Enter number = ");
-            num = sc.nextInt();
+            num = in.nextInt();
             if (num > largest_num){
                 largest_num = num;
             }
