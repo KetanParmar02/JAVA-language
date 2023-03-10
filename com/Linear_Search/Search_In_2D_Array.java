066e8c0043e308947005fc8f4c7da921a9997422
Ketan <ketan992132@gmail.com>
2023-03-10 09:06:32 +0530

    Commit message

---
+++ com/Linear_Search/Search_In_2D_Array.java
@@ -0,0 +1,45 @@
+package com.Linear_Search;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Search_In_2D_Array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Generate Random Array = ");
+        int[][] arr = new int[3][3];
+
+        // generate the random array element
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = (int) (Math.random()*100);
+            }
+        }
+
+        // print the array
+        for (int[] element:arr) {
+            System.out.println(Arrays.toString(element));
+        }
+
+        System.out.print("Enter the target element = ");
+        int target = in.nextInt();
+
+        int[] ans = search(arr, target);
+        System.out.println("The target element index value = "+Arrays.toString(ans));
+    }
+    // search target element in the 2D array
+    static int[] search(int[][] nums, int target){
+        if (nums.length == 0){
+            return new int[]{-1,-1};
+        }
+
+        for (int row = 0; row < nums.length; row++) {
+            for (int col = 0; col < nums[row].length; col++) {
+                if (nums[row][col] == target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
