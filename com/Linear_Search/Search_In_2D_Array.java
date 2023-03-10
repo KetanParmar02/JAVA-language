f24fb690445d53b13912a7d49d46210bc1127931
Ketan <ketan992132@gmail.com>
2023-03-10 09:13:59 +0530

    Commit message

--- com/Linear_Search/Search_In_2D_Array.java
+++ com/Linear_Search/Search_In_2D_Array.java
@@ -24,7 +24,7 @@ public class Search_In_2D_Array {
         System.out.print("Enter the target element = ");
         int target = in.nextInt();
 
-        int[] ans = search(arr, target);
+        int[] ans = search1(arr, target,0,2);
         System.out.println("The target element index value = "+Arrays.toString(ans));
     }
     // search target element in the 2D array
@@ -34,6 +34,22 @@ public class Search_In_2D_Array {
         }
 
         for (int row = 0; row < nums.length; row++) {
+            for (int col = 0; col < nums[row].length; col++) {
+                if (nums[row][col] == target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    // search in the 2D array in given range
+    static int[] search1(int[][] nums, int target, int start, int end){
+        if (nums.length == 0){
+            return new int[]{-1,-1};
+        }
+
+        for (int row = start; row < end; row++) {
             for (int col = 0; col < nums[row].length; col++) {
                 if (nums[row][col] == target){
                     return new int[]{row,col};
