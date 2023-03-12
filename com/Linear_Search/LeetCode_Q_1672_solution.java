ce8f500d0694f1940a4e302145ee0fe4db2b7877
Ketan <ketan992132@gmail.com>
2023-03-12 20:16:18 +0530

    Commit message

--- com/Linear_Search/LeetCode_Q_1672_solution.java
+++ com/Linear_Search/LeetCode_Q_1672_solution.java
@@ -17,8 +17,13 @@ public class LeetCode_Q_1672_solution {
         int ans = maximum_Wealth(arr1);
         System.out.println("The total bank accounts of Richest person is= "+ans);
     }
+
+    //check every person how many bank account and print the sum of all bank account
     static int maximum_Wealth(int[][] accounts){
 
+        // person = row
+        // account = col
+        // checke_every_person_bank_account_sum and return the heighest value.
         int ans = Integer.MIN_VALUE;
 
         for (int person = 0; person < accounts.length; person++) {
@@ -34,4 +39,38 @@ public class LeetCode_Q_1672_solution {
         }
         return ans;
     }
+
+    /*
+    Example 1:
+
+    Input: accounts = [[1,2,3],[3,2,1]]
+    Output: 6
+    Explanation:
+    1st customer has wealth = 1 + 2 + 3 = 6
+    2nd customer has wealth = 3 + 2 + 1 = 6
+    Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+    Example 2:
+
+    Input: accounts = [[1,5],[7,3],[3,5]]
+    Output: 10
+    Explanation:
+    1st customer has wealth = 6
+    2nd customer has wealth = 10
+    3rd customer has wealth = 8
+    The 2nd customer is the richest with a wealth of 10.
+
+    Example 3:
+
+    Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+    Output: 17
+
+
+    Constraints:
+
+    m == accounts.length
+    n == accounts[i].length
+    1 <= m, n <= 50
+    1 <= accounts[i][j] <= 100
+     */
 }
